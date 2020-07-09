d5fcd0252313c105f2b278523ad960a727ba6c7f
Anton Timonin <dotruger.37@gmail.com>
2020-07-09 14:47:56 +0300

    add IDENTITY for CS.dbo.Contracts

--- task-1/createCS1.sql
+++ task-1/createCS1.sql
@@ -6,7 +6,7 @@ CREATE DATABASE CS
 GO
 
 CREATE TABLE CS.dbo.Contracts(
-id int NOT NULL,
+id int NOT NULL IDENTITY(0, 1),
 id_car int NOT NULL,
 id_person int NOT NULL,
 id_service int NOT NULL,
@@ -64,17 +64,50 @@ date_with_date datetime NOT NULL,
 value_json VARCHAR(500) NOT NULL
 )
 
+CREATE TABLE CS.dbo.TelecomOperator (
+id int NOT NULL,
+operator VARCHAR(50) NOT NULL,
+CONSTRAINT PK_TC PRIMARY KEY(id),
+CONSTRAINT UK_TC UNIQUE(id)
+)
+
+
+CREATE TABLE CS.dbo.EquipmentMaintenance (
+id INT NOT NULL,
+id_equipment INT NOT NULL,
+id_service_center INT NOT NULL,
+date_visit DATETIME NOT NULL,
+json_report VARCHAR(150) NOT NULL,
+CONSTRAINT PK_EqM PRIMARY KEY(id),
+CONSTRAINT UK_EqM UNIQUE(id)
+)
+
+
+
+
+DROP TABLE CS.dbo.CommunicationChanel;
+
 CREATE TABLE CS.dbo.CommunicationChanel (
 id int NOT NULL,
 id_telecom_operator int NOT NULL,
-name VARCHAR(50) NOT NULL
+name VARCHAR(50) NOT NULL,
+CONSTRAINT PK_CC PRIMARY KEY(id),
+CONSTRAINT UK_CC UNIQUE(id),
+CONSTRAINT FK_id_telecom_operator FOREIGN KEY(id_telecom_operator) REFERENCES CS.dbo.TelecomOperator(id)
 )
 
-CREATE TABLE CS.dbo.TelecomOperator (
+CREATE TABLE CS.dbo.Operator (
 id int NOT NULL,
-operator VARCHAR(50) NOT NULL 
+username VARCHAR(100) NOT NULL,
+password VARCHAR(100) NOT NULL,
+CONSTRAINT PK_Operator PRIMARY KEY(id),
+CONSTRAINT UK_Operator UNIQUE(id)
 )
 
+INSERT INTO CS.dbo.Operator (id, username, password)  
+VALUES (2, 'Ljwh82', '(EFjk2hdfq');
+
+SELECT * FROM Operator as op WHERE op.username = 'antontimonin' AND op.password = '78egjer2023@';
 
 
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
@@ -87,6 +120,9 @@ name VARCHAR(50) NOT NULL,
 CONSTRAINT PK_Check PRIMARY KEY(id),
 CONSTRAINT UK_Check UNIQUE(id),
 )
+
+DROP TABLE CS.dbo.CheckForeign;
+DROP TABLE CS.dbo.CheckPrimary;
 
 CREATE TABLE CS.dbo.CheckForeign(
 id int NOT NULL,
